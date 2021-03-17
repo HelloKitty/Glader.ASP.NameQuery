bb6cb636013b72a2ca8b1968d3d196cd7a2a597c
HelloKitty <andrew.blakely@ymail.com>
2021-03-17 13:48:08 -0500

    Added NameQueryResponseCode

---
+++ src/Glader.ASP.NameQuery.Models/Codes/NameQueryResponseCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader.ASP.NameQuery
+{
+	public enum NameQueryResponseCode
+	{
+		Success = GladerEssentialsModelConstants.RESPONSE_CODE_SUCCESS_VALUE,
+
+		GeneralServerError = 2,
+
+		Invalid = 3,
+
+		UnknownEntity = 4,
+
+		Unsupported = 5
+	}
+}
