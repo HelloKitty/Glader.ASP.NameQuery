1d690fb2ff15868fa47b6bcab3772d8aa52cf3c3
HelloKitty <andrew.blakely@ymail.com>
2021-03-17 13:48:17 -0500

    Added EntityNameQueryResponse

---
+++ src/Glader.ASP.NameQuery.Models/Models/Response/EntityNameQueryResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader.ASP.NameQuery
+{
+	/// <summary>
+	/// Simplified type name for <see cref="ResponseModel{TModelType,TResponseCodeType}"/> of <see cref="string"/> and
+	/// <see cref="NameQueryResponseCode"/>.
+	/// </summary>
+	public sealed class EntityNameQueryResponse : ResponseModel<string, NameQueryResponseCode>
+	{
+		/// <inheritdoc />
+		public EntityNameQueryResponse(NameQueryResponseCode resultCode) 
+			: base(resultCode)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public EntityNameQueryResponse(string result) 
+			: base(result)
+		{
+
+		}
+	}
+}
