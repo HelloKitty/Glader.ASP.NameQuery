9eff06e605d09400d7a485f0c5fcb2c3ecaf914c
HelloKitty <andrew.blakely@ymail.com>
2021-03-17 14:44:05 -0500

    Added INameQueryServiceExtensions

---
+++ src/Glader.ASP.NameQuery.Client/Extensions/INameQueryServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+using Refit;
+
+namespace Glader.ASP.NameQuery
+{
+	public static class INameQueryServiceExtensions
+	{
+		/// <summary>
+		/// Queries for a <see cref="EntityNameQueryResponse"/> from the provided <see cref="guid"/>.
+		/// </summary>
+		/// <param name="service">The name query service.</param>
+		/// <param name="guid">The entity guid.</param>
+		/// <returns>A query response.</returns>
+		public static Task<EntityNameQueryResponse> QueryEntityNameAsync<TObjectGuidType>(this INameQueryService service, TObjectGuidType guid)
+			where TObjectGuidType : BaseGuid
+		{
+			if (service == null) throw new ArgumentNullException(nameof(service));
+
+			//We just simply forward this with the raw value.
+			return service.QueryEntityNameAsync(guid.RawValue);
+		}
+	}
+}
