7c887998c2634908aa90f0adfe13d17bbabb06ca
HelloKitty <andrew.blakely@ymail.com>
2021-03-17 13:48:25 -0500

    Added ReverseEntityNameQueryResponse

---
+++ src/Glader.ASP.NameQuery.Models/Models/Response/ReverseEntityNameQueryResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader.ASP.NameQuery
+{
+	/// <summary>
+	/// Simplified type name for <see cref="ResponseModel{TModelType,TResponseCodeType}"/> of <typeparamref name="TObjectGuidType"/> and
+	/// <see cref="NameQueryResponseCode"/>.
+	/// </summary>
+	public sealed class ReverseEntityNameQueryResponse<TObjectGuidType> : ResponseModel<TObjectGuidType, NameQueryResponseCode> 
+		where TObjectGuidType : BaseGuid
+	{
+		public ReverseEntityNameQueryResponse(NameQueryResponseCode resultCode) 
+			: base(resultCode)
+		{
+		}
+
+		public ReverseEntityNameQueryResponse(TObjectGuidType result) 
+			: base(result)
+		{
+		}
+	}
+}
