8b62f2374c9453ed9463bb1edb95212baccb8327
HelloKitty <andrew.blakely@ymail.com>
2021-03-19 01:46:21 -0500

    Append "Name" to NameQueryRoute

--- src/Glader.ASP.NameQuery.Server/Attributes/NameQueryRouteAttribute.cs
+++ src/Glader.ASP.NameQuery.Server/Attributes/NameQueryRouteAttribute.cs
@@ -8,7 +8,7 @@ namespace Glader.ASP.NameQuery
 	public class NameQueryRouteAttribute : RouteAttribute
 	{
 		public NameQueryRouteAttribute(string entityTypeName)
-			: base($"api/{entityTypeName}")
+			: base($"api/{entityTypeName}Name")
 		{
 			if (string.IsNullOrWhiteSpace(entityTypeName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(entityTypeName));
 		}
