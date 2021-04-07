459f52be6809b8bd721f4189d6034d7d216e2eda
HelloKitty <andrew.blakely@ymail.com>
2021-04-07 04:49:15 -0500

    Added container registeration extensions for EntityNameDictionary

---
+++ src/Glader.ASP.NameQuery.Client.Autofac/GladerNameQueryContainerBuilderExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Autofac;
+using Autofac.Builder;
+using Autofac.Features.Indexed;
+using Glader.ASP.ServiceDiscovery;
+using Glader.Essentials;
+
+namespace Glader.ASP.NameQuery
+{
+	public static class GladerNameQueryContainerBuilderExtensions
+	{
+		/// <summary>
+		/// Registers the specified Entity Name Dictionary type with the container.
+		/// Entity Name Dictionary type must derive from <see cref="CachedEntityNameDictionary{TObjectGuidType,TEntityEnumType}"/>.
+		/// </summary>
+		/// <typeparam name="TDictionaryType">The dictionary type.</typeparam>
+		/// <typeparam name="TGuidType">The guid type.</typeparam>
+		/// <typeparam name="TEntityType">The Entity Type enumeration type.</typeparam>
+		/// <param name="builder">The container builder.</param>
+		/// <returns>Builder</returns>
+		public static IRegistrationBuilder<TDictionaryType, ConcreteReflectionActivatorData, SingleRegistrationStyle> RegisterNameDictionaryType<TDictionaryType, TGuidType, TEntityType>(this ContainerBuilder builder)
+			where TDictionaryType : CachedEntityNameDictionary<TGuidType, TEntityType> 
+			where TGuidType : ObjectGuid<TEntityType> 
+			where TEntityType : Enum
+		{
+			if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+			return builder.RegisterType<TDictionaryType>()
+				.As<IEntityNameDictionary<TGuidType, TEntityType>>()
+				.OnActivated(args =>
+				{
+					IIndex<TEntityType, IServiceResolver<INameQueryService>> nameQueryServiceIndex = args
+						.Context.Resolve<IIndex<TEntityType, IServiceResolver<INameQueryService>>>();
+
+					//This registers all possibly entity types to the queryable entity dictionary
+					Enum.GetValues(typeof(TEntityType))
+						.Cast<TEntityType>()
+						.Select(type => new { type, resolver = GetQueryServiceOrDefault(nameQueryServiceIndex, type) })
+						.Where(r => r.resolver != null)
+						.ToList()
+						.ForEach(resolverPair =>
+						{
+							args.Instance.AddService(resolverPair.type, new NameQueryServiceResolverAdapter(resolverPair.resolver));
+						});
+				});
+		}
+
+		private static IServiceResolver<INameQueryService> GetQueryServiceOrDefault<TEntityType>(IIndex<TEntityType, IServiceResolver<INameQueryService>> nameQueryServiceIndex, TEntityType type)
+		{
+			if (nameQueryServiceIndex == null) throw new ArgumentNullException(nameof(nameQueryServiceIndex));
+			if (type == null) throw new ArgumentNullException(nameof(type));
+
+			nameQueryServiceIndex.TryGetValue(type, out var value);
+			return value;
+		}
+	}
+}
---
+++ src/Glader.ASP.NameQuery.Client.Autofac/NameQueryServiceResolverAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Glader.ASP.ServiceDiscovery;
+
+namespace Glader.ASP.NameQuery
+{
+	/// <summary>
+	/// Adapter for <see cref="IServiceResolver{TServiceInterfaceType}"/> for <see cref="INameQueryService"/>
+	/// </summary>
+	public sealed class NameQueryServiceResolverAdapter : INameQueryService
+	{
+		/// <summary>
+		/// Internal resolvable instance for the <see cref="INameQueryService"/>.
+		/// </summary>
+		private IServiceResolver<INameQueryService> ServiceResolver { get; }
+
+		public NameQueryServiceResolverAdapter(IServiceResolver<INameQueryService> serviceResolver)
+		{
+			ServiceResolver = serviceResolver ?? throw new ArgumentNullException(nameof(serviceResolver));
+		}
+
+		/// <inheritdoc />
+		public async Task<EntityNameQueryResponse> QueryEntityNameAsync(ulong id, CancellationToken token = default)
+		{
+			ServiceResolveResult<INameQueryService> result = await ServiceResolver.Create(token);
+
+			if(result.isAvailable)
+				return await result.Instance.QueryEntityNameAsync(id, token);
+
+			return new EntityNameQueryResponse(NameQueryResponseCode.GeneralServerError);
+		}
+	}
+}
