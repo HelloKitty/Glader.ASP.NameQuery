270cac8e3306ac47d7ca216a2ef3cec2a9566664
HelloKitty <andrew.blakely@ymail.com>
2021-04-07 01:01:52 -0500

    Added NameQueryServiceBaseUrlFactory

---
+++ src/Glader.ASP.NameQuery.Client/Services/NameQueryServiceBaseUrlFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader.ASP.NameQuery
+{
+	public sealed class NameQueryServiceBaseUrlFactory<TEntityTypeEnum> : IServiceBaseUrlFactory
+	{
+		public TEntityTypeEnum EntityTypeValue { get; }
+
+		public DefaultServiceBaseUrlFactory DefaultUrlFactory { get; init; } = new DefaultServiceBaseUrlFactory();
+
+		public NameQueryServiceBaseUrlFactory(TEntityTypeEnum entityTypeValue)
+		{
+			EntityTypeValue = entityTypeValue;
+		}
+
+		public string Create(Uri context)
+		{
+			if(context == null) throw new ArgumentNullException(nameof(context));
+
+			//TODO: Refactor path building to NameQuery library
+			return $"{DefaultUrlFactory.Create(context)}/api/{EntityTypeValue}Name";
+		}
+	}
+}
